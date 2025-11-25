4549943a7cd1438ec74e91c07bad2902c2096f70
stlsgg <gregory.guzey@gmail.com>
2025-11-25 10:23:47 +0300

    feat: basic functionallity done

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import sqlalchemy as alchemy
-import dotenv as de
+from sqlalchemy import text, create_engine, Engine
+from dotenv import load_dotenv
 import os
 
+# load .env variables
+load_dotenv()
+
+def init_engine(user: str, passwd: str=None) -> Engine:
+    # dialect+driver://username:password@host:port/database
+
+    if not passwd:
+        passwd = os.getenv("MARIADB_ROOT_PASSWORD", None)
+
+        if passwd is None:
+            passwd = "1234" # TODO выкидываем ошибку подключения
+
+    url = f"mariadb+mariadbconnector://{user}:{passwd}@127.0.0.1/"
+    engine = create_engine(url)
+    return engine
+
+def print_databases(engine: Engine) -> list:
+    with engine.connect() as conn:
+        res = conn.execute(text(
+            "SHOW DATABASES"
+        ))
+        print(res.all())
+
+
 def main():
-    print("hello, python")
+    engine = init_engine("root")
+    print_databases(engine)
 
 if __name__ == "__main__":
     main()
