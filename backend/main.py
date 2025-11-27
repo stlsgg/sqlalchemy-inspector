2ec7b6a0b0d35b38c686c563769356fef1342e19
stlsgg <gregory.guzey@gmail.com>
2025-11-27 09:55:33 +0300

    feat(api): create basic routes

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,68 @@
-
-
 # api
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
+
+class DatabaseOptions(BaseModel):
+    charset: str | None = None
+    collation: str | None = None
+
+class TableOptions(BaseModel):
+    charset: str | None = None
+    collation: str | None = None
 
 app = FastAPI()
 
+@app.exception_handler(StarletteHTTPException)
+def custom_http_exception_handler(req: Request, exc: StarletteHTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "status": "error",
+            "data": {
+                "error_desc": exc.detail,
+                "path": req.url.path
+            }
+        },
+    )
+
 @app.get("/databases")
 def read_databases():
-    return "databases"
+    """Get information about existing and accessable databases."""
+    # return show_dbs()
+    return {"status": "ok"}
+
+@app.post("/databases/{database_name}")
+def create_database(database_name: str, options: DatabaseOptions):
+    """Create a database with the given name and options."""
+    # TODO response code 201 if created, and some body content
+    # return create_db(database_name, options)
+    return {"status": "ok"}
 
 @app.get("/databases/{database_name}")
 def read_database(database_name: str):
-    return {"db_name": database_name }
+    """Get full information about database (metadata, tables)."""
+    return {"status": "ok"}
 
-@app.post("/databases/{database_name}")
-def create_database(database_name: str):
-    """Create a database with the given name."""
-    # TODO response code 201 if created, and some body content
-    return {"db_name": database_name }
+# TODO auth
+@app.post("/login")
+def login():
+    """Authenticate a user with given login and password."""
+    return {"status": "ok"}
 
-# TODO 404 request not found or something like that
+@app.get("/users")
+def print_users():
+    """Print all logins in the system."""
+    return {"status": "ok"}
 
-# make connect -> try to connect to specified db
-# try to connect via given user -> create form and validation
-# show tables, databases
-# in table: describe table, show all rows (limit 50, pagination)
-# parse query string -> without rest api
+@app.get("/users/me")
+def print_my_user():
+    """Print current user's login."""
+    return {"status": "ok"}
+
+@app.get("/users/{user_id}")
+def print_user(user_id: int):
+    """Print specific user's login."""
+    return {"status": "ok"}
+
