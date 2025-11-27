d74019497101e7d068976c4897003853b94a15d9
stlsgg <gregory.guzey@gmail.com>
2025-11-27 16:05:32 +0300

    feat!: a lot of changes
    forgot to make microcommits (was in a state of flow)

--- backend/db.py
+++ backend/db.py
@@ -1,38 +1,117 @@
-# Логика взаимодействия с базой данных здесь
-# Описание функций
-import sqlalchemy as alchemy
-from sqlalchemy import text, create_engine, Engine
-from dotenv import load_dotenv
-import os
+from sqlalchemy import *
+import re
 
-# load .env variables
-load_dotenv()
+class DBMSInitError(Exception):
+    pass
 
-def init_engine(user: str, passwd: str=None) -> Engine:
-    # dialect+driver://username:password@host:port/database
+class DBInitError(Exception):
+    pass
 
-    if not passwd:
-        passwd = os.getenv("MARIADB_ROOT_PASSWORD", None)
+class MyDBMS:
+    """
+    Database Management System class that provides:
+    1. Establishing a connection to a DBMS through SQLAlchemy.
+    2. Listing existing databases.
+    3. Performing CRUD operations on databases. (WIP)
+    """
+    def validate_db_name(name):
+        if not re.match(r"^[a-zA-Z0-9_]+$", name):
+            raise ValueError(f"Invalid identifier: {name}")
+        return name
 
-        if passwd is None:
-            passwd = "1234" # TODO выкидываем ошибку подключения
+    def __init__(
+        self,
+        db_conn_url: URL
+    ):
+        """Initialize connection to DBMS with a given URL object."""
 
-    url = f"mariadb+mariadbconnector://{user}:{passwd}@127.0.0.1/"
-    engine = create_engine(url)
-    return engine
+        required = {
+            "dialect": db_conn_url.get_dialect().name,
+            "driver": db_conn_url.get_dialect().driver,
+            "user": db_conn_url.username,
+            "password": db_conn_url.password,
+            "addr": db_conn_url.host,
+        }
 
-def print_databases(engine: Engine) -> list:
-    with engine.connect() as conn:
-        res = conn.execute(text(
-            "SHOW DATABASES"
-        ))
-        return res.all()
+        for arg, val in required.items():
+            if val is None or val == "":
+                raise DBMSInitError(f"empty argument given: {arg} is empty.")
 
-def show_tables(engine: Engine, db_name: str) -> list:
-    with engine.connect() as conn:
-        conn.execute(text(f"USE {db_name}"))
-        res = conn.execute(text(
-            "SHOW TABLES"
-        ))
-        return res.all()
+        self.engine = create_engine(db_conn_url)
+
+        # test connection
+        with self.engine.connect() as conn:
+            conn.execute(text("SELECT 'ping pong'"))
+
+    def show_databases(self):
+        """Show all databases on current user scope (accessable)."""
+        with self.engine.connect() as conn:
+            return [row[0] for row in conn.execute(text("SHOW DATABASES")).all()]
+
+    def use_database(self, db_name: str):
+        MyDBMS.validate_db_name(db_name)
+        with self.engine.connect() as conn:
+            return conn.execute(text(f"USE {db_name}"))
+
+class MyDatabase(MyDBMS):
+    """
+    Database class that provides:
+    1. Connecting to the specific database through the DBMS class.
+    2. Listing all tables within the database.
+    3. Performing CRUD operations on tables. (WIP)
+    """
+    def __init__(self, dbms_conn_url: URL, db_name: str) -> None:
+
+        """Initialize connection to database with a given URL object."""
+        super().__init__(dbms_conn_url)
+        self.name = MyDBMS.validate_db_name(db_name)
+        self.metadata = MetaData()
+        with self.engine.connect() as conn:
+            conn.execute(text(f"USE {self.name}"))
+
+    def show_tables(self) -> list:
+        """List all tables in a database."""
+        with self.engine.connect() as conn:
+            return [
+                row[0] for row in conn.execute(
+                    text("SHOW TABLES")
+                ).fetchall()
+            ]
+
+    def describe_table(self, table_name: str) -> list[dict]:
+        """Describe fields in a table."""
+        with self.engine.connect() as conn:
+            return conn.execute(text(f"DESCRIBE {table_name}")).mappings().all()
+
+    def show_rows(
+        self,
+        table_name: str,
+        limit: int,
+        offset: int
+    ) -> list[dict]:
+        """Select all rows in a table."""
+        table = Table(table_name, self.metadata, autoload_with=self.engine)
+
+        with self.engine.connect() as conn:
+            stmt = select(table).limit(limit).offset(offset)
+            return conn.execute(stmt).mappings().fetchall()
+
+    def describe_me(self):
+        """Describe database: charset, collation."""
+        query = text(f"""
+                SELECT
+                  SCHEMA_NAME AS Name,
+                  DEFAULT_CHARACTER_SET_NAME AS Charset,
+                  DEFAULT_COLLATION_NAME AS Collation,
+                  SCHEMA_COMMENT AS Comment
+
+                FROM information_schema.schemata
+                WHERE SCHEMA_NAME = :db_name;
+                """)
+
+        with self.engine.connect() as conn:
+            return conn.execute(
+                query,
+                {"db_name": self.name}
+            ).mappings().fetchone()
 
--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,18 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from pydantic import BaseModel
+from db import MyDatabase
+from sqlalchemy import URL
+from sqlalchemy.exc import ProgrammingError
 
 app = FastAPI()
+url = URL.create(
+    drivername="mariadb+mariadbconnector",
+    username="root",
+    password="1234",
+    host="127.0.0.1"
+)
+db = MyDatabase(url, "foobar")
 
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(req: Request, exc: StarletteHTTPException):
@@ -23,32 +33,110 @@ async def custom_http_exception_handler(req: Request, exc: StarletteHTTPExceptio
 @app.get("/databases")
 async def read_databases():
     """Get information about existing and accessable databases."""
-    # return show_dbs()
-    return {"status": "ok"}
+    databases = db.show_databases()
+    if databases is not None:
+        return {"status": "ok", "data": databases}
 
 @app.get("/databases/{database_name}")
 async def read_database(database_name: str):
-    """Get full information about database (metadata, tables)."""
-    return {"status": "ok"}
+    """Get full information about database (tables)."""
+    try:
+        db.use_database(database_name)
+        tables = db.show_tables()
+        if tables is not None:
+            return {"status": "ok", "data": tables}
+    except ValueError as err:
+        return {
+            "status": "error",
+            "desc": "ValueError occured.",
+            "detail": err,
+        }
 
-# TODO auth
-@app.post("/login")
-async def login():
-    """Authenticate a user with given login and password."""
-    return {"status": "ok"}
+    except ProgrammingError as err:
+        return {
+            "status": "error",
+            "desc": "Query error occured.",
+            "detail": err
+        }
 
-@app.get("/users")
-async def print_users():
-    """Print all logins in the system."""
-    return {"status": "ok"}
+    except Exception as err:
+        return {
+            "status": "error",
+            "desc": "Unexpected error occured.",
+            "detail": err
+        }
 
-@app.get("/users/me")
-async def print_my_user():
-    """Print current user's login."""
-    return {"status": "ok"}
+@app.get("/databases/{database_name}/tables")
+async def show_database_tables(database_name: str):
+    try:
+        db.use_database(database_name)
+        tables = db.show_tables()
+        if tables is not None:
+            return {"status": "ok", "data": tables}
+    except ValueError as err:
+        return {
+            "status": "error",
+            "desc": "ValueError occured.",
+            "detail": err,
+        }
 
-@app.get("/users/{user_id}")
-async def print_user(user_id: int):
-    """Print specific user's login."""
-    return {"status": "ok"}
+    except ProgrammingError as err:
+        return {
+            "status": "error",
+            "desc": "Query error occured.",
+            "detail": err
+        }
+
+    except Exception as err:
+        return {
+            "status": "error",
+            "desc": "Unexpected error occured.",
+            "detail": err
+        }
+
+@app.get("/databases/{database_name}/desc")
+async def show_database_description(database_name: str):
+    try:
+        db.use_database(database_name)
+        desc = db.describe_me()
+        if desc:
+            return {"status": "ok", "data": desc}
+    except ValueError as err:
+        return {
+            "status": "error",
+            "desc": "ValueError occured.",
+            "detail": err,
+        }
+
+    except ProgrammingError as err:
+        return {
+            "status": "error",
+            "desc": "Query error occured.",
+            "detail": err
+        }
+
+    except Exception as err:
+        return {
+            "status": "error",
+            "desc": "Unexpected error occured.",
+            "detail": err
+        }
+
+@app.get("/databases/{database_name}/tables/{table_name}/desc")
+async def show_table_info(database_name: str, table_name: str):
+    desc = db.describe_table(table_name)
+    if desc:
+        return {"status": "ok", "data": desc}
+
+@app.get("/databases/{database_name}/tables/{table_name}/content")
+async def select_rows_in_table(
+    database_name: str,
+    table_name: str,
+    limit: int = 50,
+    offset: int = 0
+):
+    db.use_database(database_name)
+    rows = db.show_rows(table_name, limit=limit, offset=offset)
+    if rows:
+        return {"status": "ok", "data": rows}
 
