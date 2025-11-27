7f1e7c3be46898cbd49778c6b4cbb9738dfa9284
stlsgg <gregory.guzey@gmail.com>
2025-11-27 08:36:49 +0300

    build: reorganize project structure

--- app/main.py
+++ app/main.py
@@ -40,3 +40,30 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# api
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/databases")
+def read_databases():
+    return "databases"
+
+@app.get("/databases/{database_name}")
+def read_database(database_name: str):
+    return {"db_name": database_name }
+
+@app.post("/databases/{database_name}")
+def create_database(database_name: str):
+    """Create a database with the given name."""
+    # TODO response code 201 if created, and some body content
+    return {"db_name": database_name }
+
+# TODO 404 request not found or something like that
+
+# make connect -> try to connect to specified db
+# try to connect via given user -> create form and validation
+# show tables, databases
+# in table: describe table, show all rows (limit 50, pagination)
+# parse query string -> without rest api
