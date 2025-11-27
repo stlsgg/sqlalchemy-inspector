78e12b3df1a3ff10a8adfe755de96de8787c442d
stlsgg <gregory.guzey@gmail.com>
2025-11-27 09:55:19 +0300

    refactor: move sql logic code to db.py

--- backend/main.py
+++ backend/main.py
@@ -1,45 +1,4 @@
-import sqlalchemy as alchemy
-from sqlalchemy import text, create_engine, Engine
-from dotenv import load_dotenv
-import os
 
-# load .env variables
-load_dotenv()
-
-def init_engine(user: str, passwd: str=None) -> Engine:
-    # dialect+driver://username:password@host:port/database
-
-    if not passwd:
-        passwd = os.getenv("MARIADB_ROOT_PASSWORD", None)
-
-        if passwd is None:
-            passwd = "1234" # TODO выкидываем ошибку подключения
-
-    url = f"mariadb+mariadbconnector://{user}:{passwd}@127.0.0.1/"
-    engine = create_engine(url)
-    return engine
-
-def print_databases(engine: Engine) -> list:
-    with engine.connect() as conn:
-        res = conn.execute(text(
-            "SHOW DATABASES"
-        ))
-        return res.all()
-
-def show_tables(engine: Engine, db_name: str) -> list:
-    with engine.connect() as conn:
-        conn.execute(text(f"USE {db_name}"))
-        res = conn.execute(text(
-            "SHOW TABLES"
-        ))
-        return res.all()
-
-def main():
-    engine = init_engine("root")
-    print_databases(engine)
-
-if __name__ == "__main__":
-    main()
 
 # api
 from fastapi import FastAPI
