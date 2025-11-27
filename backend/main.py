6044e20c8c28d493a779fddb87049a255560b853
stlsgg <gregory.guzey@gmail.com>
2025-11-27 10:04:03 +0300

    refactor: deleted advanced post route
    i'm not planning on making any complex api routes yet

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,6 @@ from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from pydantic import BaseModel
 
-class DatabaseOptions(BaseModel):
-    charset: str | None = None
-    collation: str | None = None
-
-class TableOptions(BaseModel):
-    charset: str | None = None
-    collation: str | None = None
-
 app = FastAPI()
 
 @app.exception_handler(StarletteHTTPException)
@@ -31,13 +23,6 @@ def custom_http_exception_handler(req: Request, exc: StarletteHTTPException):
 def read_databases():
     """Get information about existing and accessable databases."""
     # return show_dbs()
-    return {"status": "ok"}
-
-@app.post("/databases/{database_name}")
-def create_database(database_name: str, options: DatabaseOptions):
-    """Create a database with the given name and options."""
-    # TODO response code 201 if created, and some body content
-    # return create_db(database_name, options)
     return {"status": "ok"}
 
 @app.get("/databases/{database_name}")
